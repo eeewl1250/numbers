0b614256281feffebe63f6900491ba2ca5019022
Wang <eeewl1250@163.com>
2018-05-31 16:32:10 +0800

    encapsulation

--- src/converter.js
+++ src/converter.js
@@ -1,36 +1,48 @@
-//获取汇率
-var exRate;
-//我的账户似乎没有权限设置基准汇率
-//为url增加&base=CNY会返回403错误：请求不允许
-//除了USD，测试了几个都不行：EUR，CHY，CHN
-xhr("get", "https://openexchangerates.org/api/latest.json?app_id=eac7657326664018818b4bc397b32cba&base=USD", false, null, function(r) {
-    exRate = r.rates;
-});
+var cc = function() {
+    // private variables and functions
+    var exRate = 0,
+        url = "https://openexchangerates.org/api/latest.json?app_id=eac7657326664018818b4bc397b32cba&base=USD";
 
-//自定义汇率
-function userRate(uRate) {
-    for(var cname in uRate){
-        exRate[cname] = uRate[cname];
-    }
-}
+    // initialization
+    xhr("get", url, false, null, function(r) {
+        exRate = r.rates;
+    });
 
-function converter(cfrom, cto, mfrom) {
-    var rfrom = 0,
-        rto = 0;
-    for (var cname in exRate) {
-        if (cfrom == cname) {
-            rfrom = exRate[cname];
+    // public methods and attributes
+    return {
+        //获取汇率
+        getRate: function(){
+            return exRate;
+        },
+        //自定义汇率
+        userRate: function(uRate) {
+            for(var cname in uRate){
+                exRate[cname] = uRate[cname];
+            }
+        },
+        //汇率转换
+        converter: function(cfrom, cto, mfrom) {
+            var rfrom = 0,
+                rto = 0;
+            for (var cname in exRate) {
+                if (cfrom == cname) {
+                    rfrom = exRate[cname];
+                }
+                if (cto == cname) {
+                    rto = exRate[cname];
+                }
+                //找到两个汇率，直接返回结果
+                if(rfrom && rto){
+                    return mfrom / rfrom * rto;
+                }
+            }
+            //未找到对应汇率，则返回未找到的国家
+            if(rfrom || rto){
+                return (rfrom || rto) ? (rfrom ? cto : cfrom) : (cfrom + ", " + cto);
+            }
         }
-        if (cto == cname) {
-            rto = exRate[cname];
-        }
-        //找到两个汇率，直接返回结果
-        if(rfrom && rto){
-            return mfrom / rfrom * rto;
-        }
-    }
-    //未找到对应汇率，则返回未找到的国家
-    if(rfrom || rto){
-        return (rfrom || rto) ? (rfrom ? cto : cfrom) : (cfrom + ", " + cto);
-    }
-}
+    };
+}();
+
+
+
